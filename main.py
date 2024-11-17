1986ea4aec26a006956d1ea037820660b7b1f4c0
Sofia Skakun <sofiaskakun30@gmail.com>
2024-11-17 21:41:25 +0300

    main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-import json
+import json # json
 
-with open("dump.json", "r", encoding="utf-8") as file:
-    a = json.load(file)
+with open("dump.json", "r", encoding="utf-8") as file: # открываем в режиме чтения
+    a = json.load(file) # считываем в переменную a
 
-q = input("Введите номер квалификации: ")
+q = input("Введите номер квалификации: ") # вводим q
 
-found = False
-for item in a:
-    if item['fields']['code'] == q:
-        found = True
-        print("=============== Найдено ===============")
-        print(f"{item['fields']['code']} >> Специальность \"{item['fields']['title']}\", {item['fields']['c_type']}")
-if not found:
-    print("=============== Не найдено ===============")
+b = False # переменная b булевая ложная
+for i in a: # перебераем ключи в a
+    if i['fields']['code'] == q: # если равны
+        b = True # то b становится тру
+        print("=============== Найдено ===============") # выводим
+        print(f"{i['fields']['code']} >> Специальность \"{i['fields']['title']}\", {i['fields']['c_type']}") # выводим по ключам
+if not b: # если b не тру
+    print("=============== Не найдено ===============") # выводим
